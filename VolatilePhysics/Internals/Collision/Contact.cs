5eec878816796e8bc46e5809065b521e27d1ab9b
Herald <doublebarreledherald@gmail.com>
2026-02-07 19:19:38 -0600

    Format: Contact IsTrigger

--- VolatilePhysics/Internals/Collision/Contact.cs
+++ VolatilePhysics/Internals/Collision/Contact.cs
@@ -80,15 +80,11 @@ namespace Volatile
 
     internal void PreStep(Manifold manifold)
     {
+      if (IsTrigger(manifold)) return;
+      
       VoltBody bodyA = manifold.ShapeA.Body;
       VoltBody bodyB = manifold.ShapeB.Body;
 
-      if (bodyA.IsTrigger == true
-       || bodyB.IsTrigger == true
-       || manifold.ShapeA.IsTrigger == true
-       || manifold.ShapeB.IsTrigger == true)
-        return;
-      
       this.toA = this.position - bodyA.Position;
       this.toB = this.position - bodyB.Position;
       this.toALeft = this.toA.Left();
@@ -108,11 +104,7 @@ namespace Volatile
 
     internal void SolveCached(Manifold manifold)
     {
-      if (manifold.ShapeA.Body.IsTrigger == true
-       || manifold.ShapeB.Body.IsTrigger == true
-       || manifold.ShapeA.IsTrigger == true
-       || manifold.ShapeB.IsTrigger == true)
-        return;
+      if (IsTrigger(manifold)) return;
 
       this.ApplyContactImpulse(
         manifold.ShapeA.Body,
@@ -123,14 +115,11 @@ namespace Volatile
 
     internal void Solve(Manifold manifold)
     {
+
+      if (IsTrigger(manifold)) return;
+
       VoltBody bodyA = manifold.ShapeA.Body;
       VoltBody bodyB = manifold.ShapeB.Body;
-
-      if (bodyA.IsTrigger == true
-       || bodyB.IsTrigger == true
-       || manifold.ShapeA.IsTrigger == true
-       || manifold.ShapeB.IsTrigger == true)
-        return;
 
       Fix64 elasticity = bodyA.World.Elasticity;
 
@@ -168,6 +157,15 @@ namespace Volatile
 
       // Apply the normal and tangent impulse
       this.ApplyContactImpulse(bodyA, bodyB, jn, jt);
+    }
+
+    private bool IsTrigger(Manifold manifold)
+    {
+      return
+      manifold.ShapeA.Body.IsTrigger == true
+       || manifold.ShapeB.Body.IsTrigger == true
+       || manifold.ShapeA.IsTrigger == true
+       || manifold.ShapeB.IsTrigger == true;
     }
 
     #region Internals
